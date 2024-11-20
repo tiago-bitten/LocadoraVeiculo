b79e07a19113a514fd95adae7b7f00cac1b067c2
tiago.bittencourt <tiago.bittencourt@nextfit.com.br>
2024-11-20 15:27:50 -0300

    ajuste

---
+++ Locadora.Aluguel/Locadora.Aluguel/Migrations/20241120130217_AjusteIdentificador.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Locadora.Aluguel.Migrations
+{
+    /// <inheritdoc />
+    public partial class AjusteIdentificador : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DataCriacao",
+                table: "Aluguels",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Aluguels_CodigoCliente",
+                table: "Aluguels",
+                column: "CodigoCliente");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Aluguels_CodigoVeiculo",
+                table: "Aluguels",
+                column: "CodigoVeiculo");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Aluguels_CodigoCliente",
+                table: "Aluguels");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Aluguels_CodigoVeiculo",
+                table: "Aluguels");
+
+            migrationBuilder.DropColumn(
+                name: "DataCriacao",
+                table: "Aluguels");
+        }
+    }
+}
